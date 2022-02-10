547b6e87ab3704662032def8f34ede790a16969d
Chice <cko517@hotmail.com>
2022-02-09 20:55:16 -0500

    Add conditional rendering

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,26 +1,20 @@
 import React from "react";
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-  const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects"
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory
+  } = props
 
-  function categorySelected(name) {
-    console.log(`${name} clicked`)
-  }
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
@@ -33,19 +27,27 @@ function Nav() {
               About Me
             </a>
           </li>
-          <li>
-            <span>Contact</span>
+          <li className="mx-2">
+            <span onClick={() => handleClick('Contact')}>
+              Contact
+            </span>
           </li>
-            {categories.map((category) => (
-              <li
-                className="mx-1"
-                key={category.name}
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
               >
-                <span onClick={() => categorySelected(category.name)} >
-                  {category.name}
-                </span>
-              </li>
-            ))}
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
