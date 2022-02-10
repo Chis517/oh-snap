1e14f63aace84fb169508e6a28b28cd69917effb
Chice <cko517@hotmail.com>
2022-02-10 02:06:04 -0500

    Validate Form Data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
   const [formState, setFormState] = useState({
@@ -7,16 +8,35 @@ function ContactForm() {
     message: ''
   });
 
+  const[errorMessage, setErrorMessage] = useState('');
   const { name, email, message } = formState;
-
-  function handleChange(e) {
-    setFormState({...formState, [e.target.name]: e.target.value })
-  };
 
   function handleSubmit(e) {
     e.preventDefault();
     console.log(formState);
   };
+
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({...formState, [e.target.name]: e.target.value });
+    } 
+  };
+
+  console.log('errorMessage', errorMessage);
 
   return (
     <section>
